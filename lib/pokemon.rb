43636e1e30f5a559c5a33f6b2f16c8bc64f8324e
Everett <everett.bowline@gmail.com>
2018-05-30 23:54:02 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+  
+  @@all = []
+  
+  def initialize(name: name, type: type, db: db, id: id)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+ 
+  def self.save(name, type, db)
+    db.execute("INSERT INTO cats (name, type, db) VALUES (?, ?, ?)",name, type, db)
+  end
+  
+ # def self.find(id)
+    
+ # end
+  
 end
