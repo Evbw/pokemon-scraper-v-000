d295238b1a38979b07032d305ee34b12bdf0d92d
Everett <everett.bowline@gmail.com>
2018-05-31 01:42:20 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,7 @@ class Pokemon
     pokarray = db.execute("SELECT * FROM pokemon WHERE id=?", id)
     Pokemon.new(id: pokarray[0], name: pokarray[1], type: pokarray[2])
     #binding.pry
+    id
  end
   
 end
